6b934a4c6e02614fe1282e4f4a5c8e576a3c0c47
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 21:24:15 +0530

    Update urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import SignUpView, SignInView
 
+# Define URL patterns for user authentication
 urlpatterns = [
+    # Route for user registration
     path('signup/', SignUpView.as_view(), name='signup'),
+
+    # Route for user login
     path('signin/', SignInView.as_view(), name='signin'),
 ]
