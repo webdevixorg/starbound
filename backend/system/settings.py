76f1cd843807c7c5cbb5dab9d622045e5fae598e
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 19:28:37 +0530

    Update settings.py

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -22,8 +22,6 @@ import dotenv
 dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,7 +131,6 @@ WSGI_APPLICATION = 'system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -144,7 +141,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
