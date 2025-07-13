6373c160b39b144b4503dbb3b1968b1acf60c541
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 21:47:07 +0530

    Update settings.py

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -133,11 +133,11 @@ WSGI_APPLICATION = 'system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'starbounddb'),
-        'USER': os.getenv('DB_USER', 'neranjan'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
