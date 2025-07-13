7e5d201ab30173439055460405275b6bcffb67f5
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 21:47:10 +0530

    Update apps.py

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
+# Configuration class for the 'authentication' app
 class AuthenticationConfig(AppConfig):
+    # Default primary key type for models in this app (BigAutoField uses 64-bit integers)
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Name of the app as recognized by Django
     name = 'authentication'
