c64c4104ce434c6793aff09784060fd2ec58a4cf
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 23:59:25 +0530

    Update apps.py

--- backend/orders/apps.py
+++ backend/orders/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
+# Configuration class for the 'orders' app
 class OrdersConfig(AppConfig):
+    # Set the default primary key field type for models in this app
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # The name of the app; must match the app folder name
     name = 'orders'
