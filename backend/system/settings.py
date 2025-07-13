69e8dd1281e5abf53616cf9a69459a8a52d80f68
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 23:36:55 +0530

    Update settings.py

--- backend/system/settings.py
+++ backend/system/settings.py
@@ -23,7 +23,6 @@ dotenv.load_dotenv("/etc/webdevix-env/starbound.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
