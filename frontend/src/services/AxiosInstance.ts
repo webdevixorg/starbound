f8988ea0bd41e66bde24a71ba211c8b3391103ec
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-21 01:36:02 +0530

    Api Link Update

--- frontend/src/services/AxiosInstance.ts
+++ frontend/src/services/AxiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://logivis.com/api/'; // Replace with your backend URL
+const API_URL = 'http://127.0.0.1:8000/api'; // Replace with your backend URL
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
