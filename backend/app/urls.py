5904505ebbac8857f7e9032f0575528b1aa8d0f4
Neranjan Dissanayake <neranjansb@gmail.com>
2025-10-04 15:45:00 +0530

    Update App url

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,13 @@
+# system/urls.py  (main project urls.py)
 from django.urls import path, include
 
 urlpatterns = [
+    # Routes for the posts app
     path('posts/', include('app.posts.urls')),
+
+    # Routes for the FAQ app
     path('faqs/', include('app.faq.urls')),
+
+    # Routes for the product app
     path('products/', include('app.product.urls')),
 ]
