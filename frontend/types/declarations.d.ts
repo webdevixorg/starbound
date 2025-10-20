f7909445a8b7fc1f2d6d28c1c0cdc5229550c6c0
Neranjan Dissanayake <neranjansb@gmail.com>
2025-10-21 02:24:26 +0530

    Update declarations.d.ts

--- frontend/types/declarations.d.ts
+++ frontend/types/declarations.d.ts
@@ -1,15 +1,16 @@
 // Global type declarations for the frontend project
 // Add custom type definitions or module declarations below
 declare module '@splidejs/react-splide' {
-    import React from 'react';
-    import { SplideOptions } from '@splidejs/splide';
-  
-    export interface SplideProps {
-      options?: SplideOptions;
-      children?: React.ReactNode;
-    }
-  
-    export class Splide extends React.Component<SplideProps> {}
-    export class SplideSlide extends React.Component<{ children?: React.ReactNode }> {}
+  import React from 'react';
+  import { SplideOptions } from '@splidejs/splide';
+
+  export interface SplideProps {
+    options?: SplideOptions;
+    children?: React.ReactNode;
   }
-  
+
+  export class Splide extends React.Component<SplideProps> {}
+  export class SplideSlide extends React.Component<{
+    children?: React.ReactNode;
+  }> {}
+}
