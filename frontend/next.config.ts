dbb95a685819e67032a8f133dbf1da2f0657472f
Neranjan Dissanayake <neranjansb@gmail.com>
2025-10-21 00:20:00 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -108,9 +108,9 @@ const nextConfig: NextConfig = {
   // Environment variables
   env: {
     // API
-    NEXT_PUBLIC_API_URLx: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     // Supabase
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_URLx: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     // Third-party services
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
