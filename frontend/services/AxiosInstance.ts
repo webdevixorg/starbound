85b17ad1bb277a41a7ab56dd022e6a66dbb062ad
Neranjan Dissanayake <neranjansb@gmail.com>
2026-01-18 22:52:18 +0530

    Update API url

--- frontend/services/AxiosInstance.ts
+++ frontend/services/AxiosInstance.ts
@@ -1,7 +1,8 @@
 // Enhanced AxiosInstance.ts
 import axios, { AxiosError } from 'axios';
+import { NEXT_PUBLIC_API_URL } from '@/utils/env';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+const API_URL = NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
--- frontend/services/AxiosInstanceNoAuth.ts
+++ frontend/services/AxiosInstanceNoAuth.ts
@@ -1,7 +1,8 @@
 // axiosInstanceNoAuth.ts
 import axios from 'axios';
+import { NEXT_PUBLIC_API_URL } from '@/utils/env';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+const API_URL = NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 
 const axiosInstanceNoAuth = axios.create({
   baseURL: API_URL,
