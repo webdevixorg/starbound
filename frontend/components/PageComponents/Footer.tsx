ae90eb41e89d195ba66c1b0468c1b277b2821c10
Neranjan Dissanayake <neranjansb@gmail.com>
2026-01-19 23:18:11 +0530

    Update Footer.tsx

--- frontend/components/PageComponents/Footer.tsx
+++ frontend/components/PageComponents/Footer.tsx
@@ -235,17 +235,11 @@ const Footer: React.FC = () => {
       </div>
 
       {/* Start footer copyright */}
-      <div className="footer-dark">
-        <div className="container py-4 border-t border-gray-200 border-opacity-10">
-          <div className="row">
-            <div className="col-12 col-md ">
-              <p className="d-block my-3 text-center">
-                Copyright &copy; {new Date().getFullYear()} Logivis Automotive.
-                All rights reserved.
-              </p>
-            </div>
-          </div>
-        </div>
+      <div className="w-full py-4 border-t border-gray-200 border-opacity-10 text-center">
+        <p className="my-3">
+          Copyright &copy; {new Date().getFullYear()} Logivis Automotive. All
+          rights reserved.
+        </p>
       </div>
       {/* End footer copyright */}
     </footer>
